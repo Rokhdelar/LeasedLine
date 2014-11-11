e6ce7a7a1864ed24463af747884371adfea94bd4
Rokhdelar <fldx72228501>
2014-11-11 10:40:08 +0800

    Init

---
+++ src/com/flnoc/LeasedLine/Model/CommRoom.java
@@ -0,0 +1,144 @@
+package com.flnoc.LeasedLine.Model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+/**
+ * Created by Rokhdelar on 2014-11-11.
+ */
+@Entity
+public class CommRoom {
+    private int commRoomId;
+    private String commRoomName;
+    private int subId;
+    private String commRoomAddress;
+    private String commRoomContact;
+    private String commRoomPhone;
+    private String commRoomMemo;
+    private SubStation subStationBySubId;
+    private Collection<Device> devicesByCommRoomId;
+
+    @Id
+    @Column(name = "commRoomID", nullable = false, insertable = true, updatable = true)
+    public int getCommRoomId() {
+        return commRoomId;
+    }
+
+    public void setCommRoomId(int commRoomId) {
+        this.commRoomId = commRoomId;
+    }
+
+    @Basic
+    @Column(name = "commRoomName", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getCommRoomName() {
+        return commRoomName;
+    }
+
+    public void setCommRoomName(String commRoomName) {
+        this.commRoomName = commRoomName;
+    }
+
+    @Basic
+    @Column(name = "subID", nullable = false, insertable = true, updatable = true)
+    public int getSubId() {
+        return subId;
+    }
+
+    public void setSubId(int subId) {
+        this.subId = subId;
+    }
+
+    @Basic
+    @Column(name = "commRoomAddress", nullable = true, insertable = true, updatable = true, length = 100)
+    public String getCommRoomAddress() {
+        return commRoomAddress;
+    }
+
+    public void setCommRoomAddress(String commRoomAddress) {
+        this.commRoomAddress = commRoomAddress;
+    }
+
+    @Basic
+    @Column(name = "commRoomContact", nullable = true, insertable = true, updatable = true, length = 10)
+    public String getCommRoomContact() {
+        return commRoomContact;
+    }
+
+    public void setCommRoomContact(String commRoomContact) {
+        this.commRoomContact = commRoomContact;
+    }
+
+    @Basic
+    @Column(name = "commRoomPhone", nullable = true, insertable = true, updatable = true, length = 15)
+    public String getCommRoomPhone() {
+        return commRoomPhone;
+    }
+
+    public void setCommRoomPhone(String commRoomPhone) {
+        this.commRoomPhone = commRoomPhone;
+    }
+
+    @Basic
+    @Column(name = "commRoomMemo", nullable = true, insertable = true, updatable = true, length = 65535)
+    public String getCommRoomMemo() {
+        return commRoomMemo;
+    }
+
+    public void setCommRoomMemo(String commRoomMemo) {
+        this.commRoomMemo = commRoomMemo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CommRoom commRoom = (CommRoom) o;
+
+        if (commRoomId != commRoom.commRoomId) return false;
+        if (subId != commRoom.subId) return false;
+        if (commRoomAddress != null ? !commRoomAddress.equals(commRoom.commRoomAddress) : commRoom.commRoomAddress != null)
+            return false;
+        if (commRoomContact != null ? !commRoomContact.equals(commRoom.commRoomContact) : commRoom.commRoomContact != null)
+            return false;
+        if (commRoomMemo != null ? !commRoomMemo.equals(commRoom.commRoomMemo) : commRoom.commRoomMemo != null)
+            return false;
+        if (commRoomName != null ? !commRoomName.equals(commRoom.commRoomName) : commRoom.commRoomName != null)
+            return false;
+        if (commRoomPhone != null ? !commRoomPhone.equals(commRoom.commRoomPhone) : commRoom.commRoomPhone != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = commRoomId;
+        result = 31 * result + (commRoomName != null ? commRoomName.hashCode() : 0);
+        result = 31 * result + subId;
+        result = 31 * result + (commRoomAddress != null ? commRoomAddress.hashCode() : 0);
+        result = 31 * result + (commRoomContact != null ? commRoomContact.hashCode() : 0);
+        result = 31 * result + (commRoomPhone != null ? commRoomPhone.hashCode() : 0);
+        result = 31 * result + (commRoomMemo != null ? commRoomMemo.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "subID", referencedColumnName = "subID", nullable = false)
+    public SubStation getSubStationBySubId() {
+        return subStationBySubId;
+    }
+
+    public void setSubStationBySubId(SubStation subStationBySubId) {
+        this.subStationBySubId = subStationBySubId;
+    }
+
+    @OneToMany(mappedBy = "commRoomByCommRoomId")
+    public Collection<Device> getDevicesByCommRoomId() {
+        return devicesByCommRoomId;
+    }
+
+    public void setDevicesByCommRoomId(Collection<Device> devicesByCommRoomId) {
+        this.devicesByCommRoomId = devicesByCommRoomId;
+    }
+}
---
+++ src/com/flnoc/LeasedLine/Model/Device.java
@@ -0,0 +1,197 @@
+package com.flnoc.LeasedLine.Model;
+
+import javax.persistence.*;
+
+/**
+ * Created by Rokhdelar on 2014-11-11.
+ */
+@Entity
+public class Device {
+    private int deviceId;
+    private String deviceName;
+    private String deviceIp;
+    private String deviceSnmpro;
+    private String deviceSnmprw;
+    private String deviceType;
+    private String deviceModel;
+    private String deviceBras;
+    private String deviceBrasport;
+    private String deviceInternetVlan;
+    private int commRoomId;
+    private String deviceMemo;
+    private CommRoom commRoomByCommRoomId;
+
+    @Id
+    @Column(name = "deviceID", nullable = false, insertable = true, updatable = true)
+    public int getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(int deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    @Basic
+    @Column(name = "deviceName", nullable = false, insertable = true, updatable = true, length = 50)
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    @Basic
+    @Column(name = "deviceIP", nullable = false, insertable = true, updatable = true, length = 20)
+    public String getDeviceIp() {
+        return deviceIp;
+    }
+
+    public void setDeviceIp(String deviceIp) {
+        this.deviceIp = deviceIp;
+    }
+
+    @Basic
+    @Column(name = "deviceSNMPRO", nullable = false, insertable = true, updatable = true, length = 20)
+    public String getDeviceSnmpro() {
+        return deviceSnmpro;
+    }
+
+    public void setDeviceSnmpro(String deviceSnmpro) {
+        this.deviceSnmpro = deviceSnmpro;
+    }
+
+    @Basic
+    @Column(name = "deviceSNMPRW", nullable = true, insertable = true, updatable = true, length = 20)
+    public String getDeviceSnmprw() {
+        return deviceSnmprw;
+    }
+
+    public void setDeviceSnmprw(String deviceSnmprw) {
+        this.deviceSnmprw = deviceSnmprw;
+    }
+
+    @Basic
+    @Column(name = "deviceType", nullable = false, insertable = true, updatable = true, length = 10)
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    @Basic
+    @Column(name = "deviceModel", nullable = true, insertable = true, updatable = true, length = 45)
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
+    }
+
+    @Basic
+    @Column(name = "deviceBRAS", nullable = true, insertable = true, updatable = true, length = 45)
+    public String getDeviceBras() {
+        return deviceBras;
+    }
+
+    public void setDeviceBras(String deviceBras) {
+        this.deviceBras = deviceBras;
+    }
+
+    @Basic
+    @Column(name = "deviceBRASPORT", nullable = true, insertable = true, updatable = true, length = 45)
+    public String getDeviceBrasport() {
+        return deviceBrasport;
+    }
+
+    public void setDeviceBrasport(String deviceBrasport) {
+        this.deviceBrasport = deviceBrasport;
+    }
+
+    @Basic
+    @Column(name = "deviceInternetVLAN", nullable = true, insertable = true, updatable = true, length = 4)
+    public String getDeviceInternetVlan() {
+        return deviceInternetVlan;
+    }
+
+    public void setDeviceInternetVlan(String deviceInternetVlan) {
+        this.deviceInternetVlan = deviceInternetVlan;
+    }
+
+    @Basic
+    @Column(name = "commRoomID", nullable = false, insertable = true, updatable = true)
+    public int getCommRoomId() {
+        return commRoomId;
+    }
+
+    public void setCommRoomId(int commRoomId) {
+        this.commRoomId = commRoomId;
+    }
+
+    @Basic
+    @Column(name = "deviceMemo", nullable = true, insertable = true, updatable = true, length = 65535)
+    public String getDeviceMemo() {
+        return deviceMemo;
+    }
+
+    public void setDeviceMemo(String deviceMemo) {
+        this.deviceMemo = deviceMemo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Device device = (Device) o;
+
+        if (commRoomId != device.commRoomId) return false;
+        if (deviceId != device.deviceId) return false;
+        if (deviceBras != null ? !deviceBras.equals(device.deviceBras) : device.deviceBras != null) return false;
+        if (deviceBrasport != null ? !deviceBrasport.equals(device.deviceBrasport) : device.deviceBrasport != null)
+            return false;
+        if (deviceInternetVlan != null ? !deviceInternetVlan.equals(device.deviceInternetVlan) : device.deviceInternetVlan != null)
+            return false;
+        if (deviceIp != null ? !deviceIp.equals(device.deviceIp) : device.deviceIp != null) return false;
+        if (deviceMemo != null ? !deviceMemo.equals(device.deviceMemo) : device.deviceMemo != null) return false;
+        if (deviceModel != null ? !deviceModel.equals(device.deviceModel) : device.deviceModel != null) return false;
+        if (deviceName != null ? !deviceName.equals(device.deviceName) : device.deviceName != null) return false;
+        if (deviceSnmpro != null ? !deviceSnmpro.equals(device.deviceSnmpro) : device.deviceSnmpro != null)
+            return false;
+        if (deviceSnmprw != null ? !deviceSnmprw.equals(device.deviceSnmprw) : device.deviceSnmprw != null)
+            return false;
+        if (deviceType != null ? !deviceType.equals(device.deviceType) : device.deviceType != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = deviceId;
+        result = 31 * result + (deviceName != null ? deviceName.hashCode() : 0);
+        result = 31 * result + (deviceIp != null ? deviceIp.hashCode() : 0);
+        result = 31 * result + (deviceSnmpro != null ? deviceSnmpro.hashCode() : 0);
+        result = 31 * result + (deviceSnmprw != null ? deviceSnmprw.hashCode() : 0);
+        result = 31 * result + (deviceType != null ? deviceType.hashCode() : 0);
+        result = 31 * result + (deviceModel != null ? deviceModel.hashCode() : 0);
+        result = 31 * result + (deviceBras != null ? deviceBras.hashCode() : 0);
+        result = 31 * result + (deviceBrasport != null ? deviceBrasport.hashCode() : 0);
+        result = 31 * result + (deviceInternetVlan != null ? deviceInternetVlan.hashCode() : 0);
+        result = 31 * result + commRoomId;
+        result = 31 * result + (deviceMemo != null ? deviceMemo.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "commRoomID", referencedColumnName = "commRoomID", nullable = false)
+    public CommRoom getCommRoomByCommRoomId() {
+        return commRoomByCommRoomId;
+    }
+
+    public void setCommRoomByCommRoomId(CommRoom commRoomByCommRoomId) {
+        this.commRoomByCommRoomId = commRoomByCommRoomId;
+    }
+}
